ca31d59c681d7db4c8d1b1e131fc33e71576772f
yogeshkumar0588 <yogeshkumar0588@gmail.com>
2022-12-21 14:01:06 +0530

    Initial commit

---
+++ src/main/java/com/credit/suisse/CreditSuisseAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.credit.suisse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSuisseAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditSuisseAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credit/suisse/CreditSuisseAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.suisse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSuisseAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
